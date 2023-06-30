fc5de57fad7f4353f17c07b18aac75ed2fe9f2a6
Nisha George <nishajomy.2007@gmail.com>
2023-06-29 22:28:53 -0500

    created project Foodmagnet

---
+++ Foodmagnet/src/main/java/org/launchcode/Foodmagnet/FoodmagnetApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Foodmagnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmagnetApplication.class, args);
+	}
+
+}
---
+++ Foodmagnet/src/test/java/org/launchcode/Foodmagnet/FoodmagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Foodmagnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
